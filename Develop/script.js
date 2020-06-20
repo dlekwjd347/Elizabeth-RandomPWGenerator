20ede38b306be6f1f281a16f88873823f556c92e
Elizabeth Lee <dlekwjd347@gmail.com>
2020-06-19 20:51:59 -0400

    progress 6.19.20

--- Develop/script.js
+++ Develop/script.js
@@ -23,68 +23,51 @@ var needUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
 var needLowercase = "abcdefghijklmnopqrstuvwxyz";
 var needNumbers = "0123456789";
 //boolean variable for pwLength to account for situations otherwise 
-var pwLength = false;
+// var pwLength = false;
+
 
 //variable is establish but empty because it will be filled in by the user's input 
 var userPW = "";
  
 //first prompt to fulfll userPW variable
-var pwLength = prompt("Choose a length between 8 and 128")
+var pwLength = Number(prompt("Choose a length between 8 and 128"));
 
-//while statement keeps a condition for the if flow statement 
-while (pwLength < 8 || pwLength > 128 || pwLength == "") { //passwordValid is false
-  pwLength = prompt("Please choose a valid length: Choose a length between 8 and 128")
-  }
-  if (pwLength < 128 || pwLength > 8) {
-    var Specialcharacters = confirm("Do you want special characters in your password?");
-    var needUppercase = confirm("Do you want upper case letters in your password?");
-    var needLowercase = confirm("Do you want lower case letters in your password?");
-    var needNumbers = confirm("Do you want numbers in your password?");
-    
-  }
-  console.log(pwLength);
+if (pwLength) {
+  while (pwLength < 8 || pwLength > 128 || pwLength == "") { //passwordValid is false
+    pwLength = prompt("Please choose a valid length: Choose a length between 8 and 128")
+    }
+    if (pwLength < 128 || pwLength > 8) {
+      var Specialcharacters = confirm("Do you want special characters in your password?");
+      var needUppercase = confirm("Do you want upper case letters in your password?");
+      var needLowercase = confirm("Do you want lower case letters in your password?");
+      var needNumbers = confirm("Do you want numbers in your password?");
+      
+    }
+    console.log(pwLength);
+    var newPassword = "";
+      //for loop to repeatedly extract a random character or letter from the array the number of time that is specified from the userPW prompt
+      for (var i = 0; i < pwLength; i++); {
+        //create variable array of all possible pool of characters and letters to randomly generate on a loop from a single pool
+     var collectionValidCharacters = ("!#$%&()*+-/:;<>=?@^_~abcdefghijklmnopqrstuvwxyz1234567890");
+     
+     newPassword += collectionValidCharacters[Math.floor(Math.random() * collectionValidCharacters.length)];
+       console.log(newPassword);
+       document.getElementById("password").addEventListener("click", writePassword);
+     }
+  
+}
 
+return newPassword;
 
 }
 
-  //create variable array of all possible pool of characters and letters to randomly generate on a loop from a single pool
-  var collectionValidCharacters = ("!#$%&()*+-/:;<>=?@^_~abcdefghijklmnopqrstuvwxyz1234567890");
-  generatePassword = "";
-
-  //for loop to repeatedly extract a random character or letter from the array the number of time that is specified from the userPW prompt
-  for (var i = 0; i < pwLength; i++) 
-  generatePassword += collectionValidCharacters[Math.floor(Math.random() * collectionValidCharacters.length)];
-    console.log(generatePassword);
-    document.getElementById("password").addEventListener("click", writePassword);
+ 
 
     function writePassword() {
-      document.getElementById("password").innerHTML = generatePassword;
+      document.getElementById("password").innerHTML = generatePassword();
     }
 
 
 
 
    
-
-  
-  
-
-
-
-
-
-
-
-
-
-
- 
-
-  
- 
-
-
-
-
-
-
