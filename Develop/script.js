eb09774751b36d66ecf8f0b5d37eb4c965cd89a6
Elizabeth Lee <dlekwjd347@gmail.com>
2020-06-19 20:00:54 -0400

    progress 6.19.20

--- Develop/script.js
+++ Develop/script.js
@@ -15,17 +15,23 @@ generateBtn.addEventListener("click", writePassword);
 
 //--------------------------------------my code------------------------------>
 
-var Specialcharacters = "!#$%&()*+-/:;<>=?@^_~";
+//create variable name for special characters, upper case letters, lower case letters, and number arrays for confirm boxes
+function generatePassword() {
+
+  var Specialcharacters = "!#$%&()*+-/:;<>=?@^_~";
 var needUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
 var needLowercase = "abcdefghijklmnopqrstuvwxyz";
 var needNumbers = "0123456789";
+//boolean variable for pwLength to account for situations otherwise 
 var pwLength = false;
 
+//variable is establish but empty because it will be filled in by the user's input 
 var userPW = "";
  
-
+//first prompt to fulfll userPW variable
 var pwLength = prompt("Choose a length between 8 and 128")
 
+//while statement keeps a condition for the if flow statement 
 while (pwLength < 8 || pwLength > 128 || pwLength == "") { //passwordValid is false
   pwLength = prompt("Please choose a valid length: Choose a length between 8 and 128")
   }
@@ -36,11 +42,16 @@ while (pwLength < 8 || pwLength > 128 || pwLength == "") { //passwordValid is fa
     var needNumbers = confirm("Do you want numbers in your password?");
     
   }
-  // console.log(pwLength);
+  console.log(pwLength);
 
+
+}
+
+  //create variable array of all possible pool of characters and letters to randomly generate on a loop from a single pool
   var collectionValidCharacters = ("!#$%&()*+-/:;<>=?@^_~abcdefghijklmnopqrstuvwxyz1234567890");
   generatePassword = "";
 
+  //for loop to repeatedly extract a random character or letter from the array the number of time that is specified from the userPW prompt
   for (var i = 0; i < pwLength; i++) 
   generatePassword += collectionValidCharacters[Math.floor(Math.random() * collectionValidCharacters.length)];
     console.log(generatePassword);
